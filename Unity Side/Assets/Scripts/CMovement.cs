f7c025350d4720c76837dfe21515642a296d20ee
Jakob Anderson <jakob.anderson@uoit.net>
2019-10-01 13:55:18 -0400

    Undo Rework
    separated commands into singular action lists, which get mass undone on request, will help with level loading

--- Unity Side/Assets/Scripts/CMovement.cs
+++ Unity Side/Assets/Scripts/CMovement.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CMovement : BaseCommand
+public class CMovement : SingularAction
 {
     public CMovement(GameObject puppet, Vector3 position) : base(puppet)
     {
---
+++ Unity Side/Assets/Scripts/SingularAction.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class SingularAction : MonoBehaviour
+{
+    protected GameObject go = null;
+
+    protected SingularAction(GameObject puppet)
+    {
+        go = puppet;
+    }
+
+    public bool Done { get; set; } = false;
+
+    protected abstract void PerformUndo();
+
+    protected abstract void PerformRedo();
+
+    public void Undo()
+    {
+        if (Done)
+        {
+            Flip();
+            PerformUndo();
+        }
+    }
+
+    public void Redo()
+    {
+        if (!Done)
+        {
+            Flip();
+            PerformRedo();
+        }
+    }
+
+    protected void Flip()
+    {
+        Done = !Done;
+    }
+}
